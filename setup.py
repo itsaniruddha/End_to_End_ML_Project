147f633e1a7a419e9e7c1bb6e29cbffda31de2be
itsaniruddha <senguptaaniruddha99@gmail.com>
2023-09-21 15:53:59 +0530

    EDA & Problem Statement

--- setup.py
+++ setup.py
@@ -1,29 +1,27 @@
 from setuptools import find_packages,setup
 from typing import List
 
-
-hyphen_e_dot='-e .'
-def get_requirements(file_path: str) -> List[str]:
-    """
-    This function will import requirements
-    """
+HYPEN_E_DOT='-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return the list of requirements
+    '''
     requirements=[]
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
-        #when we will add requirements \n will be added 
-        # so we make next list comprehension
         requirements=[req.replace("\n","") for req in requirements]
-        
-        if hyphen_e_dot in requirements:
-            requirements.remove(hyphen_e_dot)
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    
     return requirements
 
-
 setup(
-name='ML_Project',
-version='0.01',
+name='end-to-end-ml-project',
+version='0.0.1',
 author='ani',
-author_email='itsaniruddha1999@gmail.com',
+author_email='itsaniuddha1999@gmail.com',
 packages=find_packages(),
 install_requires=get_requirements('requirements.txt')
+
 )
